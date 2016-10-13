7f783af23c5f5f13bb37a5c1496e02f44b37d4de
selliot96 <selliot96@southoldufsd.com>
2016-10-13 15:57:01 -0400

    Food Stuff

---
+++ HelloWorld/helloworld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        tuna myTuna = new tuna();
+        potpie myPotPie = new potpie();;
+        
+        myTuna.eat();
+        myPotPie.eat();
+     }
+}
